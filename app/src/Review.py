2767e8af7110c226ae131061f35628238ef1ad2f
Richard Flentie <70719085+RichardF2020@users.noreply.github.com>
2021-10-02 14:39:04 -0500

    Update Review.py

--- app/src/Review.py
+++ app/src/Review.py
@@ -9,3 +9,13 @@
 # rating provided was not between 1 and 5
 #
 # Use typing module to hint the class attributes
+
+class Review():
+    def __init__(self, restaurant_name: str, title_of_review: str, body_of_review: str, rating: int):
+        self.restaurant_name = restaurant_name
+        self.title_of_review = title_of_review
+        self.body_of_review = body_of_review
+        if(rating < 1 or rating > 5):
+            raise Exception(
+                'Invalid value for rating. Please enter a number between 1 - 5')
+        self.rating = rating
