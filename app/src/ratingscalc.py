81fb92e12f023b8eae879daf18996dfeb4f34203
Richard Flentie <70719085+RichardF2020@users.noreply.github.com>
2021-10-02 14:40:09 -0500

    Update ratingscalc.py

--- app/src/ratingscalc.py
+++ app/src/ratingscalc.py
@@ -6,3 +6,20 @@
 #
 # If the list of reviews is empty, return an empty dictionary
 # Make sure that you add type hints to the function paramter and return value
+
+def calc_avg_rating(reviews: t.List[Review]) -> dict[str, float]:
+    # treating this as an object, using == instead of =. using t/f
+    if len(reviews) == {}:
+        return {}
+
+    reviews_res: dict[str, t.List[Review]] = {}
+    for review in reviews:
+        if review.restaurant_name in reviews_res:
+            reviews_res.get(review.restaurant_name).append(review)
+        else:
+            reviews_res[review.restaurant_name] = [review]
+
+    rating_res: dict[str, t.List[float]] = {}
+    for name, list_rev in reviews_res.items():
+        rating_res[name] = calculate_avg_rating(list_rev)
+    return rating_res
